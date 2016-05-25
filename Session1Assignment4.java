2d2e275e02601c48f83d209fa4b1bfd62589b753
Pratik Ratnaparkhi <pratikr557@gmail.com>
2016-05-26 00:23:05 +0530

    Add files via upload

---
+++ Session1Assignment4.java
@@ -0,0 +1,35 @@
+
+public class Session1Assignment4 {
+
+	public static void main(String[] args) {
+		
+		/*Assume that the integer variable x is 5 and the integer variable y is 10. Give the values of the following
+		 expressions:
+		 a. x + y * 2
+		 b. x - y + 2
+		 c. (x+y) * 2
+		 d. y % x*/
+		
+		
+		int x = 5;
+		int y = 10;
+		
+		
+		
+		System.out.println(x+y*2);
+		System.out.println(x-y+2);
+		System.out.println((x+y)*2);
+		System.out.println(y%x*1); //it shows invalid expression if I entered /, so I entered 1 instead.
+		
+		
+		
+		
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
